3d5e1da8da382bdea1692c8f34803dc1db0fea51
vinoth <avinothk@outlook.com>
2014-11-22 17:21:18 +0530

    question controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all.order('created_at DESC')
+  end
+
+  def new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.save
+
+    redirect_to @question
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  private
+
+    def question_params
+      params.require(:question).permit(:content)
+    end
+end
